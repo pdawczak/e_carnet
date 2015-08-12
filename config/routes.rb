56641c8e90a99b49de8a3d706fa699ac37a0a0cb
Paweł Dawczak <pawel.dawczak@gmail.com>
2015-08-12 13:27:30 +0100

    Add simple frontend mocks

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  get 'static/index'
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
   root "static#index"
+  get "faq", to: "static#faq"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
