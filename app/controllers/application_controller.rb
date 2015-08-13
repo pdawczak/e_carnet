0461cd68a4f85def27eaf82c27b5f690a81b583d
Paweł Dawczak <pawel.dawczak@gmail.com>
2015-08-13 11:02:19 +0100

    Instantiate ECarnetApp; Give access to instiated app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  attr_reader :app
+  helper_method :app
+
+  before_filter :setup_app
+
+  private
+
+  def setup_app
+    @app = ECarnetApp.for_user_by_id(session[:user_id])
+  end
 end
