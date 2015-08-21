7d188a64dc7a22ceba30579df68e2a4a124ebdc0
Paweł Dawczak <pawel.dawczak@gmail.com>
2015-08-21 05:57:58 +0100

    Update ECarnetApp to return Users module

--- app/e_carnet_app/e_carnet_app.rb
+++ app/e_carnet_app/e_carnet_app.rb
@@ -10,4 +10,8 @@ class ECarnetApp
       new(User.find_current_user_by_id(user_id))
     end
   end
+
+  def users
+    ECarnetAppModule::Users.new(self)
+  end
 end
--- test/e_carnet_app/e_carnet_app_test.rb
+++ test/e_carnet_app/e_carnet_app_test.rb
@@ -2,6 +2,10 @@ require "test_helper"
 require "ostruct"
 
 class ECarnetAppTest < Minitest::Test
+  def app
+    @app ||= ECarnetApp.new(OpenStruct.new)
+  end
+
   def test_instantiates_new_app_with_user_id
     ECarnetApp.for_user_by_id(nil)
   end
@@ -9,5 +13,9 @@ class ECarnetAppTest < Minitest::Test
   def test_current_user
     user = OpenStruct.new
     assert_equal ECarnetApp.new(user).current_user, user
+  end
+
+  def test_users
+    assert_kind_of ECarnetAppModule::Users, app.users
   end
 end
