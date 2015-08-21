a453535ae75f1672586e88422a8e60a46ba6c5fd
Paweł Dawczak <pawel.dawczak@gmail.com>
2015-08-21 05:56:10 +0100

    Update User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
   class << self
     def find_current_user_by_id(user_id)
       return ECarnetAppModule::UsersModule::Guest.new if user_id.nil?
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,6 +1,15 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
+  def user
+    @user ||= User.new
+  end
+
+  def test_properties
+    assert_respond_to user, :email
+    assert_respond_to user, :password
+  end
+
   def test_find_current_user_by_id
     user = User.find_current_user_by_id(users(:joe).id)
     assert_kind_of User, user
