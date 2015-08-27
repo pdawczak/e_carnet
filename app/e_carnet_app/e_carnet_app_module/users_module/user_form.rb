e89cf71ec9d6ce05499d5ab44a369218c10f202c
Paweł Dawczak <pawel.dawczak@gmail.com>
2015-08-27 06:53:31 +0100

    Add UserForm for creating new Users

--- app/e_carnet_app/e_carnet_app_module/users_module/user_form.rb
+++ app/e_carnet_app/e_carnet_app_module/users_module/user_form.rb
@@ -3,7 +3,19 @@ module ECarnetAppModule
     class UserForm < Reform::Form
       property :email
       property :password
-      property :password_confirmation
+      property :password_confirmation, virtual: true
+
+      validates :email, format: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,
+                        presence: true
+      validate :passwords_valid
+
+      private
+
+      def passwords_valid
+        errors.add(:password, "can't be blank")              if password.blank?
+        errors.add(:password_confirmation, "can't be blank") if password_confirmation.blank?
+        errors.add(:password, "Passwords don't match")       unless password == password_confirmation
+      end
     end
   end
 end
