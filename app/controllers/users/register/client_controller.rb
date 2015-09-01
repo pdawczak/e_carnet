afc3048c0308ca7283ebc621fad45543c3febc19
Paweł Dawczak <pawel.dawczak@gmail.com>
2015-09-01 07:27:48 +0100

    Add support for Strong Paramteres

--- app/controllers/users/register/client_controller.rb
+++ app/controllers/users/register/client_controller.rb
@@ -5,11 +5,18 @@ class Users::Register::ClientController < ApplicationController
 
   def create
     @form = app.users.new_client_form
-    if @form.validate(params[:user])
+    if @form.validate(user_params)
       @form.save
       redirect_to root_url, notice: "Welcome #{@form.model.email}. You've been registered correctly!"
     else
       render "new"
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user)
+          .permit(:email, :password, :password_confirmation)
   end
 end
