87b5dad1fd372f794939475def33b609254025fa
Paweł Dawczak <pawel.dawczak@gmail.com>
2015-09-01 07:31:17 +0100

    Move notice generation to method

--- app/controllers/users/register/client_controller.rb
+++ app/controllers/users/register/client_controller.rb
@@ -7,13 +7,17 @@ class Users::Register::ClientController < ApplicationController
     @form = app.users.new_client_form
     if @form.validate(user_params)
       @form.save
-      redirect_to root_url, notice: "Welcome #{@form.model.email}. You've been registered correctly!"
+      redirect_to root_url, notice: notice_for(@form.model)
     else
       render "new"
     end
   end
 
   private
+
+  def notice_for(user)
+    "Welcome #{user.email}. You've been registered correctly!"
+  end
 
   def user_params
     params.require(:user)
